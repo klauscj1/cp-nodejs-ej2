a7ec88746801cd9ba7e4c4c0a3d08b171b02610d
Claus Chocho <clauschochoe@gmail.com>
2022-07-01 21:34:28 -0500

    crud-notas

--- index.js
+++ index.js
@@ -1,30 +1,87 @@
 //Verbos de http => GET,POST, PUT, DELETE, PATCH
-
+//importamos el modulo cors
+const cors = require("cors");
 //importamos el modulo express
 const express = require("express");
 //creamos un servidor
 const server = express();
+//utilizamos middleware para json
+server.use(express.json());
+//utilizamos middleware para cors
+server.use(cors());
 //puerto para el servidor
-const PORT = 3000;
+const PORT = 4000;
 
-// agregar endpoint principal=> http:localhost:3000/
+let notas = [
+  { id: 1, title: "Note 1", description: "Description note 1" },
+  { id: 2, title: "Note 2", description: "Description note 2" },
+];
+
+// agregar endpoint GET principal=> http:localhost:3000/ => http:localhost:3000
+//recuperar todas las notas existentes
 server.get("/", (req, res) => {
-  res.status(200).send({
-    saludo: "Hola",
-  });
+  res.status(200).send(notas);
 });
 
-//http:localhost:3000/saludo
-server.get("/saludo", (req, res) => {
-  res.status(200).send({
-    saludo: "Hola como estas",
-  });
+//agregar endpoint GET con id => http:localhost:3000/:id
+//recuperar el detalle de una sola nota mediante el id
+server.get("/:id", (req, res) => {
+  const { id } = req.params;
+  try {
+    const noteId = parseInt(id);
+    const note = notas.find((note) => note.id === noteId);
+    if (note) {
+      return res.status(200).send(note);
+    } else {
+      return res.status(404).send({
+        error: "No existe una nota con ese id",
+      });
+    }
+  } catch (e) {
+    return res.status(404).send({
+      error: "El id ingresado como parametro debe ser numerico",
+    });
+  }
 });
 
-server.post("/saludo", (req, res) => {
-  res.status(200).send({
-    saludo: "Hola como estas",
+//agregar endpoint POST con id => http:localhost:3000
+//crear una nueva nota
+server.post("/", (req, res) => {
+  const noteToSave = req.body;
+  const noteCompleteToSave = {
+    ...noteToSave,
+    id: notas.length + 1,
+  };
+  notas.push(noteCompleteToSave);
+  console.log("noteCompleteToSave", noteCompleteToSave);
+  res.status(201).send(noteCompleteToSave);
+});
+
+//agregar endpoint PUT con id => http:localhost:3000
+//ACTUALIZAR UNA NOTA
+server.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const noteToUpdate = req.body;
+  let noteFind = notas.find((note) => note.id === parseInt(id));
+  noteFind.title = noteToUpdate.title;
+  noteFind.description = noteToUpdate.description;
+  let newNotes = notas.map((note) => {
+    if (note.id === noteFind.id) {
+      return noteFind;
+    }
+    return note;
   });
+  res.status(200).send(noteFind);
+});
+
+//agregar endpoint DELETE con id => http:localhost:3000
+//ELIMINAR UNA NOTA
+server.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  const noteId = parseInt(id);
+  const newNotes = notas.filter((note) => note.id !== noteId);
+  notas = newNotes;
+  res.status(200).send();
 });
 
 //escuchar el servidor en el puerto PORT
